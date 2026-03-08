670907420f330cc7fbe077a3dcf59fec78ad1d81
Sarah Altouq <saltouq@bu.edu>
2026-03-07 23:27:43 -0500

    final touches, it works :

--- src/components/Structuring.tsx
+++ src/components/Structuring.tsx
@@ -2,13 +2,13 @@ import {Outlet} from 'react-router-dom'
 import Header from './Header'
 import Nav from './Nav'
 import Footer from './Footer'
-
+//outlet is a react layout component that i didnt find on the slides so thought id explain briefly, its an easier way to put the parts of the site that are gonna stay the same no matter what. Outlet basically is like a hole so where our main usually is. Since every page has the same header, nav and footer, the outlet is the hole where we just insert whatever content depending on what page we are currently on!
 export default function Structuring() {
   return (<> <Header/>
       <div id="container">
         <Nav/>
         <main>
-            <Outlet/>
+            <Outlet/> 
         </main></div><Footer/>
     </>
   )
