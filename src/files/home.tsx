4f9b8f22819c64d5e1ec94a79797808a7e9a969d
Sarah Altouq <saltouq@bu.edu>
2026-03-07 22:36:51 -0500

    added projects file that i forgot, will add certifications now

---
+++ src/files/home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h2>Home</h2>
+      <img src="Screenshot 2026-03-06 at 4.59.45 PM.png" alt="Sarah headshot" />
+      <p>With a focus on Computer Science at Boston University, my core interests lie in the intersection of technology and creativity. I am fascinated by the aesthetic appeal of computers in today’s world. Thus, I have explored the area and become a skilled computer builder, one that takes that appeal into consideration. I have honed my skills in computer graphics, utilizing both Autodesk’s Maya and Blender to create intricate graphics for video games. The ideas of which come from hundreds of hours of video game experience. Finally, I use video game engines such as Unreal Engine and Godot to create my own projects. I hope to apply my own skills and ideas to the gaming industry!</p>
+    </main>
+  )
+}
---
+++ src/files/projects.tsx
@@ -0,0 +1,18 @@
+export default function Projects() {
+  return (
+    <>
+     
+      <h2>Projects</h2>
+               <h3>ctrlBuild</h3>
+                <h3>June 2022 - Present</h3>
+                <img src="Screenshot 2026-03-06 at 6.28.01 PM.png" width="400" alt="ctrlBuild logo."/>
+                <ul>
+                    <li>Within the ever-changing world of technology, my interest is sparked by the rapid advances that characterize the field. I am motivated by a meaningful query: How can we leverage the relentless momentum of technological progress to guarantee access and involvement for all, specifically under-served communities? </li>
+                   <li> This question aligns with my dedication to enhancing digital literacy, a cause I eagerly advocate through programs like CtrlBuild. </li>
+                   <li>In high school, I founded ctrlBuild, a tech community service initiative. I started ctrlBuild because I was eager to share my passion for tech and improve tech literacy in my community. It’s my desire for kids to cultivate this same passion, as I’m confident that a generation of leaders in technology will further advance Saudi Arabia, whether through economic diversification, infrastructure, or the development of renewable energy sources. Through hands-on workshops and projects, I strive to make technology less intimidating for people from all walks of life.</li>
+
+                   <li> Through ctrlBuild, I organized workshops to improve tech skills for Saudi youth, and I taught kids who had an interest in gaming how to build their own computer keyboards. Although it was challenging dealing with finances, preparations, and managing kids, witnessing the curiosity sparkle in their eyes as they built their own keyboards was deeply fulfilling. This showed the transformative ability of hands-on STEM learning outside school, fueling my passion for improving tech literacy among Saudi youth.</li>
+                  </ul> 
+    </>
+  )
+}
